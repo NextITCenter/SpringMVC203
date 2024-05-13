2539c2ce694c9f0d49106d8cf4d7e74e4a676fb6
gks930620 <gks930620@naver.com>
2024-05-13 14:04:01 +0900

    Initial commit

---
+++ src/main/java/kr/or/nextit/springmvc/regist/RegisterRequest.java
@@ -0,0 +1,50 @@
+package kr.or.nextit.springmvc.regist;
+
+public class RegisterRequest {
+    private String email;
+    private String name;
+    private String password;
+    private String confirmPassword;
+
+    public RegisterRequest() {
+    }
+
+    public RegisterRequest(String email, String name, String password, String confirmPassword) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+<h1>
+</h1>
+<br/>
+인사말: ${greeting}
+</body>
+</html>
